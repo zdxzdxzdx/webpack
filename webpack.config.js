e8332a34db97ac6faa7882b1badb7f621e56435d
zdxzdx1219 <769371987@qq.com>
2020-12-02 08:26:04 +0800

    delete

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ var path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
   entry: "./src/index.js",
   output: {
     filename: "index.[chunkhash].js",
