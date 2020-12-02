d81509caf441311450fcf68bc4befee2cb559c5f
zdxzdx1219 <769371987@qq.com>
2020-12-02 17:16:27 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import x from "./x.js";
 import png from "./assets/1.jpeg";
-const div = document.getElementById("app");
-div.innerHTML = `
-<img src="${png}">
-`;
+// const div = document.getElementById("app");
+// div.innerHTML = `
+// <img src="${png}">
+// `;
 
 const button = document.createElement("button");
 button.innerText = "懒加载";
