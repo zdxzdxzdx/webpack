195487e963c96890dd570b593dff8976607831f8
zdxzdx1219 <769371987@qq.com>
2020-12-03 08:35:21 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import x from "./x.js";
 import png from "./assets/1.jpeg";
-// const div = document.getElementById("app");
+const div = document.getElementById("app");
 // div.innerHTML = `
 // <img src="${png}">
 // `;
